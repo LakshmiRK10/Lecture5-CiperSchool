f9c910337e06a273293994fd5e82d172fbfff3ff
LakshmiRK10 <121750915+LakshmiRK10@users.noreply.github.com>
2024-06-19 09:14:37 +0530

    Add files via upload

---
+++ conditional.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+
+    cout<<"Input 2 integers a and b";
+
+    cin>>a;
+    cin>>b;
+
+    //Statement:a>b
+    cout<<(a>b);
+   // c++ interprets true as 1
+  //  c++ interprets false as 0
+  
+
+}
---
+++ conditional2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+
+    cout<<"Input 4 integers a,b,c and d";
+
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+
+    //Statement:a>b
+    cout<<(a>b && c>d);
+}
